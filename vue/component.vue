a53c0cf15999906dfc1a2a870185e8c031869edf
QADS HOLDINGS <97460031+qads-holdings-canada@users.noreply.github.com>
2026-01-15 20:34:12 -0500

    Create component.vue

---
+++ vue/component.vue
@@ -0,0 +1,8 @@
+<template>
+  <iframe 
+    src="https://cs.qads.cloud" 
+    data-set-id="123456" // Get this in your dashboard
+    data-redirect-url="https://example.com" // Redirect URL
+    style="width:100%;border:none;"
+  ></iframe>
+</template>
