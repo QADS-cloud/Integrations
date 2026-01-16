b8b5de62fca25f076cdbeadfd93c280ee5cf623c
QADS HOLDINGS <97460031+qads-holdings-canada@users.noreply.github.com>
2026-01-15 20:43:10 -0500

    Delete nuxt directory

--- nuxt/component.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <iframe 
-    src="https://cs.qads.cloud" 
-    data-set-id="123456" <!-- Get this in your dashboard -->
-    data-redirect-url="https://example.com" <!-- Redirect URL -->
-    style="width:100%; border:none;"
-  ></iframe>
-</template>
